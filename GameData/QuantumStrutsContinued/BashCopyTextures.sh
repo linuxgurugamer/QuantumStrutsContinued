9b9b53f1ed1f767862469c7ff9086a05692ea875
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-04-28 20:41:50 -0400

    Updated for deprecated part in 1.7

---
+++ GameData/QuantumStrutsContinued/BashCopyTextures.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#
+# This script needs to be run on Linux and OSX to copy
+# the missing texture files.  Double-clicking on it
+# should be sufficient
+
+clear
+
+echo -e "\n\nThis batch file will copy the missing textures from the inaccessable"
+echo "Squad/zDeprecated directory "
+echo -e "\n\n"
+echo "Press return to continue"
+read yn
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+[ ! -d $DIR/Squad ] && mkdir $DIR/Squad
+cd $DIR/Squad
+
+mkdir linearRCS_v1
+cp ../../Squad/zDeprecated/Parts/Utility/linearRCS_v1/model.mu linearRCS_v1
+cp ../../Squad/zDeprecated/Parts/Utility/linearRCS_v1/rcs.dds linearRCS_v1
+
+
+
+echo -e "/n/nThe files have been copied/n"
+echo "Press return to continue"
+read yn
--- QuantumStrutsContinued/AssemblyVersion.cs
+++ QuantumStrutsContinued/AssemblyVersion.cs
@@ -10,4 +10,4 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.7.3.2")]
+ [assembly: AssemblyVersion("1.7.3.3")]
